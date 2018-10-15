92bb7ee1985f6f06d6d8e79b04951f0f1a45e0ce
Yuri Kunde Schlesner <yuriks@yuriks.net>
2018-10-14 17:39:53 -0700

    Experient with simplifying event loop: get rid of wakers

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,48 +1,37 @@
 use futures::future::LocalFutureObj;
+use std::cell::Cell;
 use std::cell::RefCell;
-use std::cmp;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
-use std::collections::VecDeque;
 use std::future::Future;
+use std::num::NonZeroUsize;
 use std::pin::Pin;
-use std::rc::Rc;
 use std::sync::Arc;
 use std::task;
 use std::task::LocalWaker;
 use std::task::Poll;
 use std::task::Wake;
 
+type TaskId = NonZeroUsize;
+
 thread_local! {
-    static TASK_SCHEDULER: Rc<TaskScheduler> = TaskScheduler::new();
+    static TASK_SCHEDULER: TaskScheduler = TaskScheduler::new();
+    static CURRENT_TASK: Cell<Option<TaskId>> = Cell::new(None);
 }
 
-struct WakeupEvent {
-    task_id: usize,
-    waker: LocalWaker,
-}
+struct DummyWake;
 
-struct WakeToken(usize);
-
-impl Wake for WakeToken {
-    fn wake(arc_self: &Arc<Self>) {
-        let WakeToken(task_id) = **arc_self;
-
-        TASK_SCHEDULER.with(|scheduler: &Rc<TaskScheduler>| {
-            let event = WakeupEvent {
-                task_id,
-                waker: unsafe { task::local_waker(arc_self.clone()) },
-            };
-            scheduler.run_queue.borrow_mut().push_back(event);
-        });
+impl Wake for DummyWake {
+    fn wake(_arc_self: &Arc<Self>) {
+        // ???
     }
 }
 
 struct ScheduledTask {
     scheduled_at: u64,
     scheduling_id: usize, // Used for sort tie-breaking
-    waker: LocalWaker,
+    task_id: TaskId,
 }
 
 impl Ord for ScheduledTask {
@@ -73,7 +62,7 @@ struct TaskSchedulerInner {
     scheduling_counter: usize,
     next_task_id: usize,
 
-    wait_queue: HashMap<usize, LocalFutureObj<'static, ()>>,
+    wait_queue: HashMap<TaskId, LocalFutureObj<'static, ()>>,
 }
 
 impl TaskSchedulerInner {
@@ -83,53 +72,51 @@ impl TaskSchedulerInner {
         next_id
     }
 
-    fn reschedule_task(&mut self, at: u64, lw: LocalWaker) {
+    fn reschedule_task(&mut self, at: u64, task_id: TaskId) {
         let scheduling_id = self.next_scheduling_id();
         self.scheduled_tasks.push(ScheduledTask {
             scheduled_at: at,
             scheduling_id,
-            waker: lw,
+            task_id,
         });
     }
 
-    fn alloc_task(&mut self) -> (usize, LocalWaker) {
-        let task_id = self.next_task_id;
+    fn alloc_task(&mut self) -> TaskId {
+        let task_id = NonZeroUsize::new(self.next_task_id).unwrap();
         self.next_task_id += 1;
 
-        (task_id, unsafe {
-            task::local_waker(Arc::new(WakeToken(task_id)))
-        })
+        task_id
     }
 }
 
 struct TaskScheduler {
     inner: RefCell<TaskSchedulerInner>,
-
-    run_queue: RefCell<VecDeque<WakeupEvent>>,
 }
 
 impl TaskScheduler {
-    fn new() -> Rc<TaskScheduler> {
-        Rc::new(TaskScheduler {
+    fn new() -> TaskScheduler {
+        TaskScheduler {
             inner: RefCell::new(TaskSchedulerInner {
                 current_time: 0,
                 scheduled_tasks: BinaryHeap::new(),
                 scheduling_counter: 0,
-                next_task_id: 0,
+                next_task_id: 1,
                 wait_queue: HashMap::new(),
             }),
-            run_queue: RefCell::new(VecDeque::new()),
-        })
+        }
     }
 
-    fn wait_cycles(self: &Rc<TaskScheduler>, cycles: u64) -> WaitCycles {
+    fn wait_cycles(self: &TaskScheduler, cycles: u64) -> WaitCycles {
         let inner = self.inner.borrow();
         let scheduled_time = inner.current_time + cycles;
 
-        WaitCycles { at: scheduled_time }
+        WaitCycles {
+            at: scheduled_time,
+            task_id: CURRENT_TASK.with(|current_task| current_task.get().unwrap()),
+        }
     }
 
-    fn run(self: &Rc<TaskScheduler>) {
+    fn run(self: &TaskScheduler) {
         for _ in 0..30 {
             let mut inner = self.inner.borrow_mut();
             let now = inner.current_time;
@@ -149,23 +136,24 @@ impl TaskScheduler {
                 } else {
                     break;
                 }
-                let task = inner.scheduled_tasks.pop().unwrap();
-                task.waker.wake();
-            }
+                let scheduled_task = inner.scheduled_tasks.pop().unwrap();
+                let task_id = scheduled_task.task_id;
+                let mut task = inner.wait_queue.remove(&task_id).unwrap();
 
-            let mut run_queue = self.run_queue.borrow_mut();
-            while let Some(wakeup_event) = run_queue.pop_front() {
-                let mut task = inner.wait_queue.remove(&wakeup_event.task_id).unwrap();
                 drop(inner);
-                let poll_result = {
+                let poll_result;
+                {
                     let pinned_task = Pin::new(&mut task);
-                    pinned_task.poll(&wakeup_event.waker)
+                    let waker = task::local_waker_from_nonlocal(Arc::new(DummyWake));
+                    CURRENT_TASK.with(|current_task| current_task.set(Some(task_id)));
+                    poll_result = pinned_task.poll(&waker);
+                    CURRENT_TASK.with(|current_task| current_task.set(None));
                 };
                 inner = self.inner.borrow_mut();
                 if poll_result == Poll::Pending {
-                    inner.wait_queue.insert(wakeup_event.task_id, task);
+                    inner.wait_queue.insert(task_id, task);
                 } else {
-                    println!("Finished task {}", wakeup_event.task_id);
+                    println!("Finished task {}", task_id);
                 }
             }
 
@@ -174,10 +162,10 @@ impl TaskScheduler {
         println!("Done");
     }
 
-    fn add_task(self: &Rc<TaskScheduler>, f: impl Future<Output = ()> + 'static) {
+    fn add_task(self: &TaskScheduler, f: impl Future<Output = ()> + 'static) {
         let mut inner = self.inner.borrow_mut();
 
-        let (task_id, waker) = inner.alloc_task();
+        let task_id = inner.alloc_task();
         let task = LocalFutureObj::new(Box::new(f));
 
         let now = inner.current_time;
@@ -186,7 +174,7 @@ impl TaskScheduler {
         inner.scheduled_tasks.push(ScheduledTask {
             scheduled_at: now,
             scheduling_id,
-            waker,
+            task_id,
         });
     }
 }
@@ -197,12 +185,13 @@ fn wait_cycles(cycles: u64) -> WaitCycles {
 
 struct WaitCycles {
     at: u64,
+    task_id: TaskId,
 }
 
 impl Future for WaitCycles {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, lw: &LocalWaker) -> Poll<()> {
+    fn poll(self: Pin<&mut Self>, _: &LocalWaker) -> Poll<()> {
         TASK_SCHEDULER.with(|scheduler| {
             let mut scheduler = scheduler.inner.borrow_mut();
 
@@ -210,7 +199,7 @@ impl Future for WaitCycles {
             if scheduler.current_time == self.at {
                 Poll::Ready(())
             } else {
-                scheduler.reschedule_task(self.at, lw.clone());
+                scheduler.reschedule_task(self.at, self.task_id);
                 Poll::Pending
             }
         })
@@ -225,20 +214,26 @@ async fn test_task1() {
     println!("Spam");
 }
 
+async fn other_long_fn() -> u32 {
+    println!("hi");
+    await!(wait_cycles(10));
+    println!("bye");
+    123
+}
+
 async fn test_task2() {
     await!(wait_cycles(3));
     println!("A");
     await!(wait_cycles(3));
     println!("B");
-    await!(wait_cycles(3));
-    println!("C");
+    let read_val = await!(other_long_fn());
+    println!("C: {}", read_val);
     await!(wait_cycles(3));
     println!("end");
 }
 
 pub fn scheduler_test() {
     TASK_SCHEDULER.with(|scheduler| {
-        scheduler.add_task(wait_cycles(15));
         scheduler.add_task(test_task1());
         scheduler.add_task(test_task2());
         scheduler.run();
